1ab072c7498788d6099dc5cb2e5594d7a2921bdc
MATHEUS RUSSO CAFARCCHIO <165430643+MatheusCafarchio@users.noreply.github.com>
2025-04-24 11:12:57 -0300

    Add files via upload

---
+++ Dinotopedia/jquery/fun.js
@@ -0,0 +1,153 @@
+$(function () {
+    $(".im1").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img").html('<img src="img/tirano.jpg" alt="">');
+      $(".escrita").html('<p>O <b>Tyrannosaurus rex </b>, conhecido popularmente como T-Rex, foi um dos maiores predadores terrestres que já existiram. Ele viveu no período Cretáceo Superior, há cerca de 68 a 66 milhões de anos, habitando a região que hoje corresponde à América do Norte. Com um impressionante porte físico, podia atingir entre 12 e 13 metros de comprimento e cerca de 6 metros de altura.</p>');
+    });
+  
+    $(".im2").click(function (event) {
+      event.preventDefault();
+      $(".img").html('<img src="img/Velociraptor.jpg" alt="">');
+      $(".escrita").html('<p>O <b> Velociraptor </b> foi um dinossauro pequeno, mas extremamente ágil, que viveu durante o período Cretáceo, há cerca de 75 a 71 milhões de anos. Ele habitava a região que hoje corresponde à Ásia Central, como a Mongólia. Com cerca de 2 metros de comprimento e apenas 0,5 metros de altura, era um predador eficiente e rápido.</p>');
+    });
+  
+    $(".im3").click(function (event) {
+      event.preventDefault();
+      $(".img").html('<img src="img/Triceratops.jpg" alt="">');
+      $(".escrita").html('<p> O  <b>Triceratops</b> foi um dos dinossauros herbívoros mais impressionantes do período Cretáceo Superior, há cerca de 68 a 66 milhões de anos. Ele habitava a América do Norte e podia alcançar até 9 metros de comprimento, 3 metros de altura, e pesar entre 6 e 12 toneladas.</p>');
+    });
+  
+    $(".im4").click(function (event) {
+      event.preventDefault();
+      $(".img").html('<img src="img/Stegosaurus.jpg" alt="">');
+      $(".escrita").html('<p> O <b> Stegosaurus </b> foi um herbívoro fascinante que viveu durante o período Jurássico, há cerca de 155 a 150 milhões de anos. Habitava regiões da América do Norte e Europa, alcançando até 9 metros de comprimento e 4 metros de altura, com um peso aproximado de 3 toneladas.</p>');
+    });
+  
+    $(".im5").click(function (event) {
+      event.preventDefault();
+      $(".img").html('<img src="img/Brachiosaurus.jpg" alt="">');
+      $(".escrita").html('<p> O<b> Brachiosaurus </b>foi um gigante dos períodos jurássicos, vivendo há cerca de 154 a 153 milhões de anos. Ele habitava regiões da América do Norte e era um dos maiores dinossauros conhecidos, medindo até 25 metros de comprimento, 12 metros de altura e pesando entre 30 e 40 toneladas.</p>');
+    });
+  
+    $(".im6").click(function (event) {
+      event.preventDefault();
+      $(".img").html('<img src="img/Spinosaurus.jpg" alt="">');
+      $(".escrita").html('<p> O  <b> Spinosaurus </b> foi o maior dinossauro carnívoro conhecido, vivendo durante o período Cretáceo, entre 112 e 93 milhões de anos atrás. Habitava regiões do Norte da África e podia atingir até 15 metros de comprimento, 7 metros de altura e pesar entre 7 e 9 toneladas.</p>');
+    });
+
+    $(".im21").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img2").html('<img src="img/piterrr.jpg" alt="">');
+      $(".escrita2").html('<p>O Pterossauro icônico, sem dentes, com um bico longo e uma crista proeminente na cabeça, usada possivelmente para equilíbrio no voo ou exibição. Tinha uma envergadura de até 7 metros, vivia em regiões costeiras e se alimentava de peixes capturados no mar; existiu no final do Cretáceo (86-84 milhões de anos atrás).</p>');
+    });
+
+    $(".im22").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img2").html('<img src="img/Quetzall.jpg" alt="">');
+      $(".escrita2").html('<p>O Quetzalcoatlus: O maior pterossauro já descoberto, com envergadura de 10 a 12 metros. Possuía um pescoço longo e robusto e asas gigantes, sendo um planador eficiente. Alimentava-se de pequenos animais terrestres, carniça e talvez peixes; viveu no final do Cretáceo (68-66 milhões de anos atrás)..</p>');
+    });
+
+    $(".im23").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img2").html('<img src="img/rham.jpg" alt="">');
+      $(".escrita2").html('<p>O Rhamphorhynchus: Pequeno pterossauro com cauda longa terminando em uma ponta losangular, usada para estabilizar o voo. Tinha envergadura de 1 a 2 metros, dentes curvados para capturar peixes, habitava regiões aquáticas e viveu no final do Jurássico (150 milhões de anos atrás).</p>');
+    });
+
+    $(".im24").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img2").html('<img src="img/dimorrr.png" alt="">');
+      $(".escrita2").html('<p>O Dimorphodon: Reconhecido por sua cabeça grande e dentes afiados, com dois tipos de dentes especializados para capturar presas. Era pequeno, com envergadura de 1 metro, voava curtas distâncias e caçava insetos e pequenos animais em florestas e áreas costeiras; viveu no início do Jurássico (200 milhões de anos atrás).</p>');
+    });
+
+    $(".im25").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img2").html('<img src="img/tapejarr.jpg" alt="">');
+      $(".escrita2").html('<p>O Tapejara: Pterossauro de tamanho médio com uma crista óssea marcante e colorida, que provavelmente tinha função de exibição social ou aerodinâmica. Possuía envergadura de 3 a 4 metros, era onívoro, alimentando-se de frutas e peixes, e planava com eficiência; viveu no início do Cretáceo (112 milhões de anos atrás).</p>');
+    });
+
+    $(".im31").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img3").html('<img src="img/mosa.jpg" alt="">');
+      $(".escrita3").html('<p>O Mosasaurus: Réptil marinho de grande porte, chegava a 15 metros de comprimento, com corpo hidrodinâmico e mandíbulas poderosas repletas de dentes afiados, usado para caçar peixes, tartarugas e até outros répteis marinhos. Viveu no final do Cretáceo (70-66 milhões de anos atrás).</p>');
+    });
+
+    $(".im32").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img3").html('<img src="img/pli.avif" alt="">');
+      $(".escrita3").html('<p>O Pliosaurus: Um dos maiores predadores dos mares, com cerca de 13 metros de comprimento e mandíbulas incrivelmente fortes. Tinha nadadeiras grandes para impulsão e caçava presas grandes, incluindo outros répteis; viveu no final do Jurássico (155-150 milhões de anos atrás)..</p>');
+    });
+    
+    $(".im33").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img3").html('<img src="img/ich.jpg" alt="">');
+      $(".escrita3").html('<p>O Ichthyosaurus: Semelhante a um golfinho moderno, media cerca de 3 metros e era extremamente rápido, com corpo aerodinâmico e grandes olhos adaptados para enxergar em águas profundas. Alimentava-se de peixes e lulas; viveu no início do Jurássico (200-190 milhões de anos atrás).</p>');
+    });
+
+    $(".im34").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img3").html('<img src="img/krono.jpg" alt="">');
+      $(".escrita3").html('<p>O Kronosaurus: Réptil marinho do grupo dos pliossauros, tinha cerca de 10 metros de comprimento, com nadadeiras potentes e um crânio enorme repleto de dentes cônicos, ideal para esmagar cascos de tartarugas e corpos de presas maiores. Viveu no início do Cretáceo (125-99 milhões de anos atrás).</p>');
+    });
+
+    $(".im35").click(function (event) {
+      event.preventDefault(); // Impede o recarregamento da página
+      $(".img3").html('<img src="img/elas.jpg" alt="">');
+      $(".escrita3").html('<p>O Elasmosaurus: Plesiossauro famoso por seu pescoço incrivelmente longo, que podia medir até 7 metros dos 14 totais do corpo. Usava o pescoço flexível para capturar peixes em águas rasas, enquanto permanecia camuflado; viveu no final do Cretáceo (80-65 milhões de anos atrás).</p>');
+    });
+
+
+
+    $("#terra").click(function (event) {
+      event.preventDefault(); // Previne o comportamento padrão
+  
+      // Calcula a altura do menu fixo
+      var menuHeight = $(".header").outerHeight(); // Substitua "nav" pelo seletor do menu
+  
+      // Seleciona a div de destino
+      var target = $(".linha");
+  
+      // Faz o scroll considerando o deslocamento do menu
+      $("html, body").animate({
+          scrollTop: target.offset().top - menuHeight
+      }, 800); // Duração do movimento (800ms)
+  });
+   
+    $("#voar").click(function (event) {
+      event.preventDefault(); // Previne o comportamento padrão
+  
+      // Calcula a altura do menu fixo
+      var menuHeight = $(".header").outerHeight(); // Substitua "nav" pelo seletor do menu
+  
+      // Seleciona a div de destino
+      var target = $(".linha2");
+  
+      // Faz o scroll considerando o deslocamento do menu
+      $("html, body").animate({
+          scrollTop: target.offset().top - menuHeight
+      }, 800); // Duração do movimento (800ms)
+  });
+  
+  $("#agua").click(function (event) {
+    event.preventDefault(); // Previne o comportamento padrão
+    
+    // Calcula a altura do menu fixo
+    var menuHeight = $(".header").outerHeight(); // Substitua "nav" pelo seletor do menu
+
+    // Seleciona a div de destino
+    var target = $(".linha3");
+
+    // Faz o scroll considerando o deslocamento do menu
+    $("html, body").animate({
+        scrollTop: target.offset().top - menuHeight
+    }, 800); // Duração do movimento (800ms)
+    
+});
+
+
+
+
+
+
+
+  });
+  
